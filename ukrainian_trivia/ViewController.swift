b1d17a5376e6a27b36429e2b9a56042d981b8ed0
Dustin Wurtz <dtwurtz@gmail.com>
2016-08-24 16:00:01 -0700

    Created simply interface for pre ukrainian trivia

---
+++ ukrainian_trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ukrainian_trivia
+//
+//  Created by Dustin Wurtz on 8/24/16.
+//  Copyright © 2016 Dustin Wurtz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
