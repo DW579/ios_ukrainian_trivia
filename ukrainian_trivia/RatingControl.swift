8e6fba4cb6ca62b437580f2026d071a04eef0396
Dustin Wurtz <dtwurtz@gmail.com>
2016-08-26 16:16:44 -0700

    Fixed spacing

--- ukrainian_trivia/RatingControl.swift
+++ ukrainian_trivia/RatingControl.swift
@@ -5,12 +5,14 @@ class RatingControl: UIView {
     
     var rating = 0
     var ratingButtons = [UIButton]()
+    let spacing = 5
+    let starCount = 5
     
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
             let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
             button.backgroundColor = UIColor.redColor()
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
@@ -25,9 +27,10 @@ class RatingControl: UIView {
     override func layoutSubviews() {
         var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
         
+        
         // Offset each button's origin by the length of the button plus spacing.
         for (index, button) in ratingButtons.enumerate() {
-            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            buttonFrame.origin.x = CGFloat(index * (44 + spacing))
             button.frame = buttonFrame
         }
     }
